8729b90b1f47eee2ebed8773e0be9e51fdbd44b8
Jürgen Viljaste <j.viljaste@gmail.com>
2014-12-07 20:39:55 +0200

    Update

--- src/build/modules/memcached/manifests/packages.pp
+++ src/build/modules/memcached/manifests/packages.pp
@@ -1,13 +1,8 @@
 class memcached::packages {
-  exec { 'apt-get update':
-    path => ['/usr/bin']
-  }
-
   package {[
       'build-essential',
       'libevent-dev'
     ]:
-    ensure => present,
-    require => Exec['apt-get update']
+    ensure => present
   }
 }
