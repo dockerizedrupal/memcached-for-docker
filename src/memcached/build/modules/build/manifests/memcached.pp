fc5fe322f732909a4fa8e7444a2d72d6805dc743
Jürgen Viljaste <j.viljaste@gmail.com>
2015-06-28 02:06:39 +0300

    Update

--- src/memcached/build/modules/build/manifests/memcached.pp
+++ src/memcached/build/modules/build/manifests/memcached.pp
@@ -6,13 +6,17 @@ class build::memcached {
     source => 'puppet:///modules/build/tmp/memcached-1.4.24.tar.gz'
   }
 
-  bash_exec { 'cd /tmp && tar xzf memcached-1.4.24.tar.gz':
+  bash_exec { 'cd /tmp && gunzip memcached-1.4.24.tar.gz':
     require => File['/tmp/memcached-1.4.24.tar.gz']
+  }
+
+  bash_exec { 'cd /tmp && tar xf memcached-1.4.24.tar':
+    require => File['cd /tmp && gunzip memcached-1.4.24.tar.gz']
   }
 
   bash_exec { 'cd /tmp/memcached-1.4.24 && ./configure':
     timeout => 0,
-    require => Bash_exec['cd /tmp && tar xzf memcached-1.4.24.tar.gz']
+    require => Bash_exec['cd /tmp && tar xf memcached-1.4.24.tar']
   }
 
   bash_exec { 'cd /tmp/memcached-1.4.24 && make':
